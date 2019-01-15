66f1388adcc0e2d85e98925598c12920a0503d3e
Hendi Santika <hendisantika@gmail.com>
2019-01-16 06:15:11 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/kotlin/springbootblogkotlin/SpringBootBlogKotlinApplication.kt
@@ -0,0 +1,12 @@
+package com.hendisantika.kotlin.springbootblogkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootBlogKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootBlogKotlinApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/hendisantika/kotlin/springbootblogkotlin/SpringBootBlogKotlinApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.hendisantika.kotlin.springbootblogkotlin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBootBlogKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
+
